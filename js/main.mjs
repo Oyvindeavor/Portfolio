9fe0639b315905b8d66b739b273bc17addb8a931
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-06-09 01:11:33 +0200

    Update main.mjs

--- js/main.mjs
+++ js/main.mjs
@@ -4,11 +4,7 @@ import { transitionPage } from "../js/utils/pageTransitions.mjs";
 document.addEventListener("DOMContentLoaded", () => {
     animateName();
     transitionPage();
-    disableScroll();
     }
 );
 
 // Prevents scrolling on the homepage as i dont want scrolling on the homepage
-function disableScroll() {
-    document.addEventListener("touchmove", preventDefault, { passive: false });
-}
